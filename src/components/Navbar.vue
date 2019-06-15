e6737eb2e68e5ff909edb513b9611d345c8aab90
Jordan Levin <jleevinn@gmail.com>
2019-06-15 16:13:30 -0700

    Initial bulma navbar setup

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,53 +1,77 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <p class="navbar-brand">{{ leagueName }}</p>
-    <button
-      class="navbar-toggler"
-      type="button"
-      data-toggle="collapse"
-      data-target="#navbarNav"
-      aria-controls="navbarNav"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-    >
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item active">
-          <router-link class="nav-link" to="/">{{ links[0] }}</router-link>
-          <!-- <a class="nav-link" href="#">
-            Home
-            <span class="sr-only">(current)</span>
-          </a>-->
-        </li>
-        <li class="nav-item">
-          <!-- the code below makes it refresh the page when clicked -->
-          <!-- <a class="nav-link" href="/about">Divisions</a> -->
+  <nav class="navbar" id="boxshadow" role="navigation" aria-label="main navigation">
+  <div class="navbar-brand">
+    <a class="navbar-item" href="https://bulma.io">
+      <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
+    </a>
 
-          <!-- the code below doesnt make it refresh the page once clicked  -->
-          <router-link class="nav-link" to="/divisions">
-            {{ links[1] }}
-          </router-link>
-        </li>
-        <li class="nav-item">
-          <router-link class="nav-link" to="/schedules">
-            {{ links[2] }}
-          </router-link>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/#">{{ links[3] }}</a>
-        </li>
-      </ul>
+    <a role="button" @click="toggleHamburger" class="navbar-burger burger" :class="{'is-active': hamburgerToggled}" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+    </a>
+  </div>
+
+  <div id="navbarBasicExample" class="navbar-menu" :class="{'is-active': hamburgerToggled}">
+    <div class="navbar-start">
+      <a class="navbar-item">
+        Home
+      </a>
+
+      <a class="navbar-item">
+        Divisions
+      </a>
+
+      <!-- <div class="navbar-item has-dropdown is-hoverable">
+        <a class="navbar-link">
+          More
+        </a>
+
+        <div class="navbar-dropdown">
+          <a class="navbar-item">
+            About
+          </a>
+          <a class="navbar-item">
+            Jobs
+          </a>
+          <a class="navbar-item">
+            Contact
+          </a>
+          <hr class="navbar-divider">
+          <a class="navbar-item">
+            Report an issue
+          </a>
+        </div>
+      </div> -->
     </div>
-  </nav>
+
+    <div class="navbar-end">
+      <div class="navbar-item">
+        <div class="buttons">
+          <a class="button is-primary">
+            <strong>Sign up</strong>
+          </a>
+          <a class="button is-light">
+            Log in
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</nav>
 </template>
 
 <script>
 export default {
   name: "NavaBar",
+  methods: {
+    toggleHamburger() {
+      this.hamburgerToggled = !this.hamburgerToggled
+    }
+  },
   data() {
     return {
+      hamburgerToggled: false,
       leagueName: "Sunday Futbol League",
       links: ["Home", "Divisions", "Schedules", "Nothing"]
     };
@@ -55,4 +79,10 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style lang="scss" scoped>
+#boxshadow {
+  border: 1px solid;
+  padding: 10px;
+  box-shadow: 5px 1px;
+}
+</style>
